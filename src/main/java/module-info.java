cdb3dd39c71c509782e2aa3448632df601903eba
Bastian Åström <ba222ji@student.lnu.se>
2024-03-27 10:39:56 +0100

    Initial commit for SakilaDatabase

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.astrom.sakiladatabase {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.astrom.sakiladatabase to javafx.fxml;
+    exports com.astrom.sakiladatabase;
+}
